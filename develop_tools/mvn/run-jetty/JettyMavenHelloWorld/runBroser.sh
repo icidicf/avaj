df24e3c51ec73f0b8701dd80d5fefa9e6ce8a7ce
lyp <icidicf@gmail.com>
2015-10-16 21:09:30 -0700

    add jetty test

---
+++ develop_tools/mvn/run-jetty/JettyMavenHelloWorld/runBroser.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+firefox -new-window http://localhost:8090
---
+++ develop_tools/mvn/run-jetty/JettyMavenHelloWorld/runServer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#the port is specified in source code 
+#https://wiki.eclipse.org/Jetty/Tutorial/Jetty_and_Maven_HelloWorld 
+mvn clean compile exec:java
